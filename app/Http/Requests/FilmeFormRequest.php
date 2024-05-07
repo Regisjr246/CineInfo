12dd8266af8c937d24c74565fe03e56f6c18ce58
Regisjr246 <regisjr246@gmail.com>
2024-05-07 15:54:13 -0300

    up

--- app/Http/Requests/FilmeFormRequest.php
+++ app/Http/Requests/FilmeFormRequest.php
@@ -38,6 +38,7 @@ class FilmeFormRequest extends FormRequest
 
     public function failedValidation(Validator $validator)
     {
+        
         throw new HttpResponseException(response()->json([
             'success' => false,
             'error' => $validator->errors()
