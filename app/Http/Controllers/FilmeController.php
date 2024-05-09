d9e578d9184ea04c5d3f38dda3788ad751bb5790
Regisjr246 <regisjr246@gmail.com>
2024-05-09 08:11:09 -0300

    Finalizado Projeto

--- app/Http/Controllers/FilmeController.php
+++ app/Http/Controllers/FilmeController.php
@@ -3,19 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\FilmeFormRequest;
+use App\Http\Requests\FilmeFormRequestUpdate;
 use App\Models\Filme;
 use Illuminate\Http\Request;
 
 class FilmeController extends Controller
 {
-
     // Cadastro de filme
     public function cadastroFilme(FilmeFormRequest $request)
     {
-     
         $filme = Filme::create([
 
-            'titulo' => $request->titulo , 
+            'titulo' => $request->titulo,
             'diretor' => $request->diretor,
             'genero' => $request->genero,
             'dt_lancamento' => $request->dt_lancamento,
@@ -32,40 +31,102 @@ class FilmeController extends Controller
         ], 200);
     }
 
-    
+    // pesquisar de filme
+    public function pesquisa(Request $request)
+    {
+        $query = Filme::query();
+        $query->where(function ($q) use ($request) {
+            $q->where('sinopse', 'like', '%' . $request->input('pesquisa') . '%')
+                ->orWhere('genero', 'like', '%' . $request->input('pesquisa') . '%');
+        })
+            ->where('titulo', 'like', '%' . $request->input('pesquisa') . '%');
+        $filmes = $query->get();
+        if (count($filmes) > 0) {
+            return response()->json([
+                'status' => true,
+                'data' => $filmes
+            ]);
+        }
+        return response()->json([
+            'status' => false,
+            'data' => "Nenhum resultado encontrado"
+        ]);
+    }
 
+    //FUNÇÃO DE EXCLUIR
+    public function deletarFilme($filmes)
+    {
+        $filmes = Filme::find($filmes);
+        if (!isset($filmes)) {
+            return response()->json([
+                'status' => false,
+                'message' => "Filme não encontrado"
+            ]);
+        }
+        $filmes->delete();
+        return response()->json(([
+            'status' => true,
+            'message' =>  "Filme excluido com sucesso"
+        ]));
+    }
 
-
-public function pesquisa(Request $request)
-{
-    $query = Filme::query();
-
-    $query->where(function ($q) use ($request) {
-        $q->where('sinopse', 'like', '%' . $request->input('pesquisa') . '%')
-          ->orWhere('genero', 'like', '%' . $request->input('pesquisa') . '%');
-    })
-    ->where('titulo', 'like', '%' . $request->input('pesquisa') . '%');
-
-    $filmes = $query->get();
-
-    if (count($filmes) > 0) {
+    //Listagem
+    public function retornarTodos()
+    {
+        $filmes =  Filme::all();
+        if (!isset($filmes)) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Não há registros no sistema'
+            ]);
+        }
         return response()->json([
             'status' => true,
             'data' => $filmes
         ]);
     }
+    //FUNÇÃO DE UPDATE
+    public function updatefilmes(FilmeFormRequestUpdate $request)
+    {
+        $filmes = Filme::find($request->id);
+        if (!isset($filmes)) {
+            return response()->json([
+                'status' => false,
+                'message' => 'filmes não encontrado'
+            ]);
+        }
+        if (isset($request->titulo)) {
+            $filmes->titulo = $request->titulo;
+        }
+        if (isset($request->diretor)) {
+            $filmes->diretor = $request->diretor;
+        }
+        if (isset($request->genero)) {
+            $filmes->genero = $request->genero;
+        }
+        if (isset($request->dt_lancamento)) {
+            $filmes->dt_lancamento = $request->dt_lancamento;
+        }
+        if (isset($request->duracao)) {
+            $filmes->duracao = $request->duracao;
+        }
 
-    return response()->json([
-        'status' => false,
-        'data' => "Nenhum resultado encontrado"
-    ]);
-}
-
-
-
-
-
-
-
-
+        if (isset($request->sinopse)) {
+            $filmes->sinopse = $request->sinopse;
+        }
+        if (isset($request->elenco)) {
+            $filmes->elenco = $request->elenco;
+        }
+        if (isset($request->classificacao)) {
+            $filmes->classificacao = $request->classificacao;
+        }
+        if (isset($request->plataformas)) {
+            $filmes->plataformas = $request->plataformas;
+        }
+        $filmes->update();
+        return response()->json([
+            'status' => true,
+            'message' => 'filmes ataulizado'
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FilmeController;
-Route::post('cadastro/Filme', [FilmeController::class, 'cadastroFilme']);
-Route::post('pesquisa/Filme', [FilmeController::class, 'pesquisa']);
+
+
+
+
+route::post('filmes/cadastro', [FilmeController::class, 'cadastroFilme']);
+route::get('filmes/listagem', [filmeController::class, 'retornarTodos']);
+route::get('filmes/pesquisar', [filmeController::class, 'pesquisa']);
+
+route::delete('filmes/delete/{id}', [filmeController::class, 'deletarFilme']);
+route::put('filmes/update', [filmeController::class, 'updatefilmes']);
